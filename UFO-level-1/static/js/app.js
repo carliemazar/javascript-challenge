cb5df9f3deacbe7754b01dc02e2b766f9381e9ac
carliemazar <azar.carlie@gmail.com>
2020-09-24 14:48:27 -0400

    console logged data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,8 @@
 // from data.js
 var tableData = data;
 
+console.log(data);
+
 // YOUR CODE HERE!
 var $tbody = document.querySelector("tbody");
 var $date = document.querySelector("#datetime");
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,6 +5,8 @@ var tableData = data;
 // from data.js
 var tableData = data;
 
+console.log(data);
+
 // YOUR CODE HERE!
 var $tbody = document.querySelector("tbody");
 var $date = document.querySelector("#datetime");
@@ -55,3 +57,26 @@ function resetData() {
 
 // Render the table on page load
 renderTable();
+
+// Begin adding different filter options
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
